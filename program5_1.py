1680700a896d41546c9694916f8b45914e7fc812
swethasamy <75251257+swethasamy@users.noreply.github.com>
2022-06-19 22:46:51 +0530

    Add files via upload

---
+++ program5_1.py
@@ -0,0 +1,10 @@
+import numpy as np
+ 
+arr1 = np.array([3, 2, 1])
+arr2 = np.array([1, 2, 3])
+   
+print ("1st array : ", arr1)  
+print ("2nd array : ", arr2)  
+   
+out_arr = np.add(arr1, arr2)  
+print ("added array : ", out_arr)
