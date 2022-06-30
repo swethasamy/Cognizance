18b5c578200ec267a74956e2e8e838cd39f0f6e6
swethasamy <75251257+swethasamy@users.noreply.github.com>
2022-06-30 06:26:00 +0530

    Add files via upload

---
+++ task3/progam4.py
@@ -0,0 +1,11 @@
+name = "amrita school of engineering chennai campus"
+ 
+print(name.capitalize())
+name1 = "amrita"
+name2 = "school"
+name3 = "of"
+name4 = "engineering"
+name5 = "chennai"
+name6 = "campus"
+print(name1.capitalize() + name2.capitalize()
+                         + name3.capitalize() + name4.capitalize() + name5.capitalize() + name6.capitalize())
